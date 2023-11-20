a261ecc87ba73b0cad5d8b154dcac22b2db98861
ganeshpc <gnesh.pc@gmail.com>
2023-11-20 22:51:03 +0530

    First commit

---
+++ src/test/java/dev/ganeshpc/paymentservice/PaymentserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ganeshpc.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
